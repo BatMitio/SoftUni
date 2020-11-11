8b37c77568ecfecfea235e5f941083b3c2915821
BatMitio <mitko_zlia@abv.bg>
2020-11-11 18:49:23 +0200

    Initial commit of Spring Data Intro

---
+++ SpringData/SpringDataIntro/Lab/demo/src/main/java/com/example/demo/entities/Account.java
@@ -0,0 +1,27 @@
+package com.example.demo.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NonNull
+    private BigDecimal balance;
+
+    @ManyToOne
+    @ToString.Exclude
+    private User user;
+}
---
+++ SpringData/SpringDataIntro/Lab/demo2/src/main/java/entities/Account.java
@@ -0,0 +1,27 @@
+package entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NonNull
+    private BigDecimal balance;
+
+    @ManyToOne
+    @ToString.Exclude
+    private User user;
+}
